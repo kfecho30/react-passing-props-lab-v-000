5852adad2af110eafe7f46fd98bdaf63016067fa
Kelsey Fecho <kfecho30@gmail.com>
2018-07-31 13:25:51 +0000

    automatically backed up by learn

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
-
 import FruitBasket from './FruitBasket';
 
-const App = () => <FruitBasket />;
+//const App = () => <FruitBasket />;
+
+class App extends React.Component{
+  constructor(){
+
+  }
+
+  render(){
+    return(<FruitBasket />)
+  }
+}
 
 export default App;
